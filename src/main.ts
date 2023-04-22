4f6ecebb5cbbb27c97d412f3b49cc7c25aa7429c
Adam Bannach <adam.bannach@grafana.com>
2023-04-22 10:34:31 -0500

    feat(wip): begin pulling in pieces of action

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,64 @@
 import * as core from '@actions/core'
+import exec from '@actions/exec'
+import github from '@actions/github'
 import {wait} from './wait'
+import fs from 'fs'
+import path from 'path'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token = core.getInput('token')
+    const stageAllFiles = core.getInput('stage-all-files')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const context = github.context
+    const octokit = github.getOctokit(token)
+
+    // Get the root directory for the repository
+    const {
+      stdout: rootDir,
+      stderr: rootDirErr,
+      exitCode: rootDirExitCode
+    } = await exec.getExecOutput('git', ['rev-parse', '--show-toplevel'])
+
+    if (stageAllFiles === 'true') {
+      await exec.exec('git', ['add', '.'], {cwd: rootDir})
+    }
+
+    // Get only staged files
+    const {
+      stdout: diffString,
+      stderr: diffErr,
+      exitCode: diffExitCode
+    } = await exec.getExecOutput(
+      'git',
+      ['diff', '--staged', '--name-only', 'HEAD'],
+      {cwd: rootDir}
+    )
+
+    // Split the output into an array of files
+    const diff = diffString
+      .split('\n')
+      .map(f => f.trim())
+      .filter(f => f !== '')
+
+    // Generate the tree
+    const tree = diff.map(_file => {
+      return {
+        path: _file,
+        mode: '100644', // non-executable blob
+        type: 'blob',
+        content: fs.readFileSync(path.join(rootDir, _file), {
+          encoding: 'utf-8'
+        })
+      }
+    })
+
+    const createTreePayload = {
+      owner: 'grafana',
+      repo: 'cloud-data-archiver-plugin',
+      base_tree: `${process.env.LATEST_SHA}`,
+      tree
+    }
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
