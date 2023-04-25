2e0c2237ac82f52afa9ba23fc3ef3807f71f950e
abannachGrafana <113929542+abannachGrafana@users.noreply.github.com>
2023-04-25 13:49:14 -0500

    chore(cleanup): Readme updates, remove unused code (#6)
    * chore(cleanup): remove unused code; update readme; change version of package.json to 0.1.0
    
    * chore(test): remove verbose flag

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
-import * as exec from '@actions/exec'
 import type {ExecOptions} from '@actions/exec'
+import * as exec from '@actions/exec'
 import * as github from '@actions/github'
 import fs from 'fs'
 import path from 'path'
